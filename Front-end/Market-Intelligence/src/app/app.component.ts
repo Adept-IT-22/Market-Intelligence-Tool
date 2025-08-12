89cb92a6ba8e3d6344c1276cd0af668dbdb34ed1
adolfiscariot <m10mathenge@gmail.com>
2025-08-12 12:13:43 +0300

    feat: sidebar component done

--- Front-end/Market-Intelligence/src/app/app.component.ts
+++ Front-end/Market-Intelligence/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./@shared/components/navbar/navbar.component";
+import { SidebarComponent } from "./@shared/components/sidebar/sidebar.component";
 
 @Component({
   selector: 'app-root',
