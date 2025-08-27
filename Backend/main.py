0bbd14f7ccba01232f96767c608d6a3cbe77a04e
adolfiscariot <m10mathenge@gmail.com>
2025-08-27 09:06:45 +0300

    refactor: ensure the get_table_schema function only reads from the 'Market' table once using a regular sql call

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request 
+from flask_cors import CORS
 import time
 import os
 from dotenv import load_dotenv
@@ -6,49 +7,53 @@ import logging
 from agent_manager import AgentManager
 from typing import Dict
 
-logger = logging.getLogger()
+#Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
 
 app = Flask(__name__)
+CORS(app)
 
 #========QDRANT CONFIGS=========
 load_dotenv()
 host = os.getenv("QDRANT_HOST", "localhost")
 port = os.getenv("QDRANT_PORT", "8000")
+
+#Just Checking If The Correct URL Is Being Called
 qdrant_url = os.getenv("QDRANT_URL", f"http://{host}:{port}")
-
-print(f"Connecting to Qdrant at {qdrant_url}")
-
+logger.info(f"Connecting to Qdrant at {qdrant_url}")
 
 #query="I'm a farmer in Limuru and want to explore selling my excess maize stock. How might I go about doing that and am i in the right location?")
 
-@app.route('/query', methods=["GET", "POST"])
+@app.route('/query', methods=["GET"])
 def run_query()->Dict:
     start_time = time.perf_counter()
     logger.info("Running Query...")
 
     user_query = request.args.get("q")
+
     if not user_query:
         logger.error("No query found")
         return {"error": "Missing query"}, 400
 
     logger.info(f"Running query: {user_query}")
+    
     try:
         manager = AgentManager(query=user_query)
         results = manager.pipeline()
 
         logger.info("============QUERY RESULTS==========")
         logger.info(results)
+        logger.info("====================================")
         
         duration = time.perf_counter() - start_time
         logger.info(f"This task took {duration:.2f} seconds")
-        return {"results": results}
+        logger.info(f"Type of results['text'] is: {type(results)}")
+        return {"Results": str(results)}
 
     except Exception as e:
         logger.error(f"Couldn't run the query: {str(e)}")
-        return {"error": str(e)}, 500
-
-    
+        return {"Error": str(e)}, 500
 
 if __name__ == "__main__":
     logger.info("App starting...")
--- Front-end/Market-Intelligence/src/app/@shared/components/main-search/main-search.component.ts
+++ Front-end/Market-Intelligence/src/app/@shared/components/main-search/main-search.component.ts
@@ -1,15 +1,42 @@
-import { Component } from '@angular/core';
-
-interface Message {
-  text: string;
-  sender: 'user' | 'bot';
-}
+import { HttpClient } from '@angular/common/http';
+import { Component, Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+import { FormsModule } from '@angular/forms'
+import { CommonModule } from '@angular/common';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-main-search',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, MarkdownModule],
   templateUrl: './main-search.component.html',
   styleUrl: './main-search.component.scss'
 })
+
 export class MainSearchComponent {
+  query: string = ""
+  response: any;
+
+  constructor(private http: HttpClient){}
+
+  sendQuery(){
+    //If there's no query exit
+    if (!this.query.trim()) return;
+
+    console.log(this.query)
+
+    this.http.get(
+      `${environment.apiUrl}/query`, 
+      {params: {q: this.query}}
+    ).
+      subscribe({
+        next: (response) => {
+          console.log('Response:', response);
+          this.response = response;
+        },
+        error: (err) => {
+          console.error('Error:', err);
+        }
+      })
+  }
 }
